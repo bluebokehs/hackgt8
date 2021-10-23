06ac20da62b39dacd612da6f24628b1c58b33069
mikewcm <90279864+mikewcm@users.noreply.github.com>
2021-10-23 18:09:19 -0400

    popup balance changes

--- materials.js
+++ materials.js
@@ -9,6 +9,18 @@ class Box {
     addBoxes(a) {
         this.amountBoxes += a;
     }
+    getCompany() {
+        console.log(this.company);
+        return this.company;
+    }
+    getAmountBoxes() {
+        console.log(this.amountBoxes);
+        return this.amountBoxes;
+    }
+    useBox(k) {
+        k.useKeys();
+        this.amountBoxes -= 1;
+    }
     //Developer Method
     displayBoxType() {
         console.log("Box Name: " + this.company + ". Amount: " + this.amountBoxes + ".");
@@ -23,7 +35,11 @@ class Key {
         this.amountKeys += 1;
     }
     getAmount() {
+        console.log(this.amountKeys);
         return this.amountKeys;
+    }
+    useKeys() {
+        this.amountKeys -= 1;
     }
 }
 
@@ -36,4 +52,12 @@ class Discount {
     }
 }
 
-k = new Key(10);
+k = new Key(13);
+boxes = [];
+boxes[0] = new Box("Target", 2);
+boxes[1] = new Box("Amazon", 3);
+boxes[2] = new Box("Walmart", 2);
+boxes[3] = new Box("Chick-fil-a", 1)
+boxes[4] = new Box("Khol's", 0);
+boxes[5] = new Box("McDonald's", 0);
+boxes[6] = new Box("Steam", 0);
