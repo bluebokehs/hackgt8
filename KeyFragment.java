80fed7698907f51c6f63d249f81b7ee678fc4f10
Rohbiar <73039664+Rohbiar@users.noreply.github.com>
2021-10-23 15:03:39 -0400

    Delete KeyFragment.java

--- KeyFragment.java
+++
@@ -1,8 +0,0 @@
-public class KeyFragment extends Key {
-    private int keyFragID;
-
-    public KeyFragment(String name, int keyFragID) {
-        super(name);
-        this.keyFragID = keyFragID;
-    }
-}
