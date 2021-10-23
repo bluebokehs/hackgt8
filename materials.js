4f4b933aa80bacf256a8d4d1e973bc47ee339512
mikewcm <90279864+mikewcm@users.noreply.github.com>
2021-10-23 17:00:26 -0400

    c3

---
+++ materials.js
@@ -0,0 +1,38 @@
+class Box {
+    constructor(
+        company,
+        amountBoxes
+    ) {
+        this.company = company;
+        this.amountBoxes = amountBoxes;
+    }
+    addBoxes(a) {
+        this.amountBoxes += a;
+    }
+    //Developer Method
+    displayBoxType() {
+        console.log("Box Name: " + this.company + ". Amount: " + this.amountBoxes + ".");
+    }
+}
+
+class Key {
+    constructor(amountKeys) {
+        this.amountKeys = amountKeys;
+    }
+    addKeys() {
+        this.amountKeys += 1;
+    }
+    getAmount() {
+        return this.amountKeys;
+    }
+}
+
+class Discount {
+    constructor(
+        company,
+        percentage_off
+    ) {
+
+    }
+}
+
