69d837b23a97fc049bd99c31b31dbcb9d5a1ecaf
mikewcm <90279864+mikewcm@users.noreply.github.com>
2021-10-24 01:07:37 -0400

    added video modal

---
+++ won_items.js
@@ -0,0 +1,17 @@
+function toggleElement() {
+    var x = document.getElementById("myDIV");
+    x.style.display = "block";
+}
+
+function randomGift() {
+    let per = Math.random() * 61;
+    document.getElementById("myDIV").innerHTML = Math.round(per) + "% Off Target Coupon!"
+}
+
+var delayInMilliseconds = 6000; //1 second
+
+setTimeout(function() {
+    toggleElement();
+    randomGift();
+    console.log("done");
+}, delayInMilliseconds);
